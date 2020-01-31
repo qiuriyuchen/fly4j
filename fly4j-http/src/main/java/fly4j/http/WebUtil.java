7a279a26b42c7053629f5c34f184210710b0ed82
qiuriyuchen <qiuriyuchen@aliyun.com>
2020-01-31 20:19:33 +0800

    add webUtil

---
+++ fly4j-http/src/main/java/fly4j/http/WebUtil.java
@@ -0,0 +1,73 @@
+package fly4j.http;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+
+/**
+ * web层的一些小功能封装
+ *
+ * @author xxgw
+ */
+public class WebUtil {
+    private static final Log log = LogFactory.getLog(WebUtil.class);
+
+    public static PrintWriter getPrintWriter(HttpServletResponse resp) {
+        PrintWriter writer = null;
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html; charset=utf-8");
+        try {
+            writer = resp.getWriter();
+        } catch (Exception e) {
+        }
+        return writer;
+    }
+
+    public static boolean getParameterBoolean(HttpServletRequest request, String paramName) {
+        String value = request.getParameter(paramName);
+        return "true".equals(value);
+
+    }
+
+
+
+
+    public String getIp(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (StringUtils.isBlank(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+    public boolean isFromPhone(HttpServletRequest req) {
+        String userAgent = req.getHeader("user-agent").toLowerCase();
+        if (userAgent.contains("iphone"))
+            return true;
+        if (userAgent.toLowerCase().contains("android"))
+            return true;
+        return false;
+    }
+
+    public boolean isFromPc(HttpServletRequest req) {
+        return !isFromPhone(req);
+    }
+
+
+
+    public void sendRedirect(HttpServletResponse resp, String returnUrl) {
+        try {
+            resp.sendRedirect(URLDecoder.decode(returnUrl, "utf-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
