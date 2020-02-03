fb1e7374d0d00669c7a3645ccaae430372f7595d
qiuriyuchen <qiuriyuchen@aliyun.com>
2020-02-03 19:37:31 +0800

    add file info

---
+++ fly4j-common/src/main/java/fly4j/common/file/FileInfo.java
@@ -0,0 +1,92 @@
+package fly4j.common.file;
+
+
+import fly4j.common.DateUtil;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static fly4j.common.file.BroserUtil.convertFileSize;
+
+/**
+ * 用来前台展示文件信息
+ *
+ * @author guanpanpan
+ */
+public class FileInfo {
+    public static DateFormat dateFormat = DateFormat.getDateTimeInstance();
+
+
+
+    //相对于下载网址的路径
+    public final String fileRelativePath;
+    public final boolean pic;
+    private final String displaySize;
+    //文件名称
+    public final String fileName;
+    //文件绝对路径
+    public final String absolutePath;
+    public final String lastModified;
+    public final boolean isDirectory;
+
+    //扩展属性
+    public Map<String, Object> extMap = new HashMap<>();
+
+    public FileInfo(File file) {
+        //绝对路径
+        this.absolutePath = file.getAbsolutePath();
+        this.pic = FileUtil.isImg(file.getPath());
+        //相对路径
+        String absolutePathLinux = file.getAbsolutePath().replaceAll(
+                "\\\\", "/");
+        if (absolutePathLinux.split("/pic/").length > 1) {
+            this.fileRelativePath = absolutePathLinux.split("/pic/")[1];
+        } else {
+            this.fileRelativePath = absolutePathLinux;
+        }
+
+        this.fileName = file.getName();
+        this.displaySize = FileUtils.byteCountToDisplaySize(file.length());
+        long time = file.lastModified();
+        this.lastModified = DateUtil.formatStrinCommon(new Date(time));
+
+        isDirectory = file.isDirectory();
+        if (!isDirectory) {
+            extMap.put("size", file.length());
+            extMap.put("manSize", convertFileSize(file.length()));
+
+        }
+        String date = dateFormat.format(new Date(file.lastModified()));
+        extMap.put("date", date);
+
+        String type = "File"; // This String will tell the extension of the file
+        if (file.isDirectory()) type = "DIR"; // It's a DIR
+        else {
+            String tempName = file.getName().replace(' ', '_');
+            if (tempName.lastIndexOf('.') != -1) type = tempName.substring(
+                    tempName.lastIndexOf('.')).toLowerCase();
+        }
+        extMap.put("type", type);
+
+
+
+    }
+
+    @Override
+    public String toString() {
+        return fileRelativePath;
+    }
+
+    public String getFileRelativePathPic() {
+        return "/pic/" + fileRelativePath;
+    }
+    public boolean isPacked(){
+       return BroserUtil.isPacked(absolutePath, true);
+    }
+
+
+}
