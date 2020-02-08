65bd0d6af64a9aeee6b5b5f1ad6a12f7e71bcd79
qiuriyuchen <qiuriyuchen@aliyun.com>
2020-02-08 19:33:22 +0800

    add test

---
+++ fly4j-common/src/test/java/fly4j/common/TestDateUtil.java
@@ -0,0 +1,33 @@
+package fly4j.common;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author guanpanpan
+ */
+@RunWith(BlockJUnit4ClassRunner.class)
+public class TestDateUtil {
+
+    @Before
+    public void setup() throws Exception {
+
+    }
+
+    @Test
+    public void updateBlog() throws Exception {
+        Assert.assertEquals(0, DateUtil.getBetweenDay("2015-05-14", "2015-05-14"));
+        assertEquals(13, DateUtil.getBetweenDay("2015-05-01", "2015-05-14"));
+        assertEquals(13, DateUtil.getBetweenDay("2015-05-14", "2015-05-01"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
