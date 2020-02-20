bb2dbac5d7db7ebb51037a29a67646160f5d226b
guan <qiuriyuchen@aliyun.com>
2020-02-20 22:21:09 +0800

    delete zip

--- fly4j-common/src/main/java/fly4j/common/track/TrackContext.java
+++ fly4j-common/src/main/java/fly4j/common/track/TrackContext.java
@@ -5,23 +5,25 @@ package fly4j.common.track;
  * Created by guanpanpan on 2018-01-11.
  */
 public class TrackContext {
-    private static ThreadLocal<TrackContext> threadLocal=new ThreadLocal<TrackContext>();
-    public static void init() {
-        if(null==threadLocal.get()){
-            threadLocal.set(new TrackContext());
-        }
+    public static String SPLIT = "----";
+    private static ThreadLocal<TrackContext> threadLocal = new ThreadLocal<TrackContext>();
 
+
+    public static void reset() {
+        threadLocal.set(new TrackContext());
     }
-    /**业务跟踪*/
+
+    /**
+     * 业务跟踪
+     */
     private StringBuilder trackInfoStrBuilder = new StringBuilder();
 
     public static String getTrackInfo() {
-        init();
         return threadLocal.get().trackInfoStrBuilder.toString();
     }
 
     public static void appendTrackInfo(String iknowInfoAppend) {
-        init();
-        threadLocal.get().trackInfoStrBuilder.append(iknowInfoAppend).append(";   ");
+        threadLocal.get().trackInfoStrBuilder.append(iknowInfoAppend).append(SPLIT);
+        System.out.println(iknowInfoAppend);
     }
 }
