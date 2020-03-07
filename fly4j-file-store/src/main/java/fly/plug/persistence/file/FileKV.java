58b7076b8b95872869216ec581a8250312d45bc8
guan <qiuriyuchen@aliyun.com>
2020-03-07 22:59:22 +0800

    add file test

--- fly4j-file-store/src/main/java/fly/plug/persistence/file/FileKV.java
+++ fly4j-file-store/src/main/java/fly/plug/persistence/file/FileKV.java
@@ -1,12 +1,25 @@
 package fly.plug.persistence.file;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.List;
 
 /**
+ * dataDir 数据存储的目录,比如路径为/export/data；下边可以存放不同数据，通过pin和innerDir来区分，可以理解为不通数据库
+ * pin   可以理解为不同的数据库
+ * secondDbSpace 细分数据,可以为空,比如存放文章和用户，分别为article和user，则对应数据目录分别为/export/data/article和/export/data/user
+ * <p>
+ * bseDir
+ * ------/pin
+ * ------/pin/article/
+ * ------/pin/user/
+ * ------/pin2
+ * ------/pin/article/
+ * 如果pin和innerDir为空，则退化为全局数据Map
+ * bseDir 数据文件
  * fileKv's floder
- * bseDir=/
+ * <p>
  * db1
  * key1[json1]
  * key2[json2]
@@ -18,45 +31,57 @@ import java.util.List;
 public class FileKV {
     private FileStrStore fileStrStore = new FileStrStore();
 
+    private String dataDir;
+    private String secondDbSpace;
 
-    public FileKV(String baseDir, String innerDir) {
-        reset(baseDir, innerDir);
+    public FileKV(String dataDir) {
+        reset(dataDir, null);
     }
 
-    public void reset(String baseDir, String innerDir) {
-        this.baseDir = baseDir;
-        this.innerDir = innerDir;
+    public FileKV(String dataDir, String secondDbSpace) {
+        reset(dataDir, secondDbSpace);
     }
 
-    private String baseDir;
-    private String innerDir;
+    public void reset(String baseDir, String secondDbSpace) {
+        this.dataDir = baseDir;
+        this.secondDbSpace = secondDbSpace;
+    }
 
-
-    public Long setValue(String pin, String id, String value) {
-
-        String storePath = getStoreFilePath(pin, id);
+    /**
+     * @param
+     * @param id    主键，可以理解为Map的key
+     * @param value
+     * @return
+     */
+    public Long setValue(String firstDbSpace, String id, String value) {
+        String storePath = getStoreFilePath(firstDbSpace, id);
         fileStrStore.setValue(storePath, value);
-
         return null;
     }
 
-    public List<String> getValues(String pin, String keyPre) {
-        return fileStrStore.getValues(getStoreDirPath(pin), keyPre);
+    public List<String> getValues(String firstDbSpace, String keyPre) {
+        return fileStrStore.getValues(getStoreDirPath(firstDbSpace), keyPre);
     }
 
-    public String getValue(String pin, String id) {
-        String filePath = getStoreFilePath(pin, id);
+    public String getValue(String firstDbSpace, String id) {
+        String filePath = getStoreFilePath(firstDbSpace, id);
         return fileStrStore.getValue(filePath);
     }
 
-    public void delete(String pin, String id) {
-        String filePath = getStoreFilePath(pin, id);
+    public void delete(String firstDbSpace, String id) {
+        String filePath = getStoreFilePath(firstDbSpace, id);
         fileStrStore.delete(filePath);
     }
 
 
-    private String getStoreDirPath(String pin) {
-        return FilenameUtils.concat(FilenameUtils.concat(baseDir, pin), innerDir);
+    private String getStoreDirPath(String firstDbSpace) {
+        if (StringUtils.isBlank(firstDbSpace)) {
+            return dataDir;
+        }
+        if (StringUtils.isBlank(secondDbSpace)) {
+            return FilenameUtils.concat(dataDir, firstDbSpace);
+        }
+        return FilenameUtils.concat(FilenameUtils.concat(dataDir, firstDbSpace), secondDbSpace);
     }
 
     public String getStoreFilePath(String pin, String id) {
