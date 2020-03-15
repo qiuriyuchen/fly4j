722516273530def45b244e81ebd13359bc9c71fb
guan <qiuriyuchen@aliyun.com>
2020-03-15 13:53:49 +0800

    encryptDir

--- fly4j-file-back/src/main/java/fly4b/back/DirEncrypt.java
+++ fly4j-file-back/src/main/java/fly4b/back/DirEncrypt.java
@@ -1,13 +1,17 @@
 package fly4b.back;
 
+import fly4j.common.JsonUtils;
 import fly4j.common.file.FileNameUtilHelper;
 import fly4j.common.file.FileUtil;
 import fly4j.common.test.TestData;
 import fly4j.crypto.MD5Util;
 import fly4j.crypto.XorUtil;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,25 +23,34 @@ public class DirEncrypt {
     public Map<String, String> encrypt(String srcDirUrl, String targetDirUrl, int pass) {
         String baseDir = new File(srcDirUrl).getAbsolutePath() + File.separator;
         Map<String, String> fileNameMap = new HashMap<>();
-        innnerEncrypt(new File(srcDirUrl), targetDirUrl, baseDir, pass, fileNameMap);
+        innnerEncrypt(new File(srcDirUrl), targetDirUrl, baseDir, pass, fileNameMap, new Integer(0));
+        try {
+            FileUtils.writeStringToFile(new File(FilenameUtils.concat(targetDirUrl, "index.fbkindex")), JsonUtils.writeValueAsString(fileNameMap), Charset.forName("utf-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return fileNameMap;
     }
 
 
-    private void innnerEncrypt(File srcDir, String targetDirUrl, String baseDir, int pass, Map<String, String> fileNameMap) {
+    private void innnerEncrypt(File srcDir, String targetDirUrl, String baseDir, int pass, Map<String, String> fileNameMap, Integer index) {
         try {
             File[] files = srcDir.listFiles();
 
             for (File file : files) {
                 if (file.isDirectory()) {
                     //递归
-                    innnerEncrypt(file, targetDirUrl, baseDir, pass, fileNameMap);
+                    innnerEncrypt(file, targetDirUrl, baseDir, pass, fileNameMap, index);
                 } else {
                     //生成md5
                     String key = FileUtil.getRelativeStandardPath(file, baseDir);
                     long md5 = MD5Util.ketamaHash(key);
-                    XorUtil.encryptFile(file.getAbsolutePath(), FilenameUtils.concat(targetDirUrl, md5 + ".fbk"), pass);
+                    String fbkFile = FilenameUtils.concat(targetDirUrl, "" + (index / 100));
+                    fbkFile = FilenameUtils.concat(fbkFile, md5 + ".fbk");
+                    FileUtils.forceMkdirParent(new File(fbkFile));
+                    XorUtil.encryptFile(file.getAbsolutePath(), fbkFile, pass);
                     fileNameMap.put(md5 + ".fbk", key);
+                    index++;
                 }
 
             }
@@ -48,8 +61,17 @@ public class DirEncrypt {
 
     }
 
-    public void reStore(String srcDir, String targetDirUrl, int pass, Map<String, String> fileNameMap) {
-        this.reStore(new File(srcDir), targetDirUrl, pass, fileNameMap);
+    public void reStore(String srcDir, String targetDirUrl, int pass) {
+        //取得上次的md5Map
+        File md5File = new File(FilenameUtils.concat(srcDir, "index.fbkindex"));
+        try {
+            String md5Str = FileUtils.readFileToString(md5File, Charset.forName("utf-8"));
+            HashMap<String, String> fileNameMap = JsonUtils.readStringStringHashMap(md5Str);
+            this.reStore(new File(srcDir), targetDirUrl, pass, fileNameMap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void reStore(File srcDir, String targetDirUrl, int pass, Map<String, String> fileNameMap) {
@@ -58,15 +80,20 @@ public class DirEncrypt {
 
             for (File file : files) {
                 if (file.isDirectory()) {
-                    continue;
+                    reStore(file, targetDirUrl, pass, fileNameMap);
+                } else {
+                    if (FilenameUtils.getExtension(file.getName()).equals("fbk")) {
+                        //解密存放目录
+                        String storeName = fileNameMap.get(file.getName());
+                        String storeFullPath = FilenameUtils.concat(targetDirUrl, storeName);
+                        //解密
+                        FileUtil.forceMkdir(FilenameUtils.getFullPath(storeFullPath));
+                        XorUtil.decryptFile(file.getAbsolutePath(), storeFullPath, pass);
+                    }
+
                 }
 
-                //解密存放目录
-                String storeName = fileNameMap.get(file.getName());
-                String storeFullPath = FilenameUtils.concat(targetDirUrl, storeName);
-                //解密
-                FileUtil.forceMkdir(FilenameUtils.getFullPath(storeFullPath));
-                XorUtil.decryptFile(file.getAbsolutePath(), storeFullPath, pass);
+
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -75,13 +102,14 @@ public class DirEncrypt {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        FileUtils.forceDelete(new File(TestData.tTargetDir));
         String targetDirEncrypt = FilenameUtils.concat(TestData.tTargetDir, "dirEncrypt");
-        String targetDirEncryptResore = FilenameUtils.concat(targetDirEncrypt, "restore");
+        String targetDirEncryptResore = FilenameUtils.concat(TestData.tTargetDir, "dirEncryptRestore");
         FileUtil.forceMkdir(targetDirEncrypt);
         FileUtil.forceMkdir(targetDirEncryptResore);
         DirEncrypt dirEncrypt = new DirEncrypt();
-        Map<String, String> fileNameMap = dirEncrypt.encrypt(FilenameUtils.concat(TestData.tSourceDir, "dirEncrypt"), targetDirEncrypt, 123);
-        dirEncrypt.reStore(targetDirEncrypt, targetDirEncryptResore, 123, fileNameMap);
+        dirEncrypt.encrypt(FilenameUtils.concat(TestData.tSourceDir, "dirEncrypt"), targetDirEncrypt, 123);
+        dirEncrypt.reStore(targetDirEncrypt, targetDirEncryptResore, 123);
     }
 }
