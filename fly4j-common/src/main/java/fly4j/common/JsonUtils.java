083337a6650d2e179337c54418b50f404a86d9dc
guan <qiuriyuchen@aliyun.com>
2020-05-23 13:50:56 +0800

    update jackson and log4j2

--- fly4j-common/src/main/java/fly4j/common/JsonUtils.java
+++ fly4j-common/src/main/java/fly4j/common/JsonUtils.java
@@ -1,12 +1,11 @@
 package fly4j.common;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -18,41 +17,29 @@ public class JsonUtils {
     private static final Log log = LogFactory.getLog(JsonUtils.class);
     public static final String DATEFORMAT = "yyyy-MM-dd HH:mm:ss";
     public static ObjectMapper objectMapper = new ObjectMapper();
-    static{
-//        objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//
-//        SerializationConfig serConfig = objectMapper.getSerializationConfig();
-//        serConfig.setDateFormat(new SimpleDateFormat(DATEFORMAT));
-//        serConfig.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
-//        DeserializationConfig deserializationConfig = objectMapper.getDeserializationConfig();
-//        deserializationConfig.setDateFormat(new DateFormatUtil(DATEFORMAT));
-//
-//        objectMapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
-//
-//        //for quoted
-//        objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
-//        objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-//        objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
-//        objectMapper.configure(JsonParser.Feature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER, true);
-//        objectMapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+
+    static {
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
     }
-    public static String writeValueAsString(Object entity){
+
+    public static String writeValueAsString(Object entity) {
         try {
             return objectMapper.writeValueAsString(entity);
-        }catch (Exception e) {
+        } catch (Exception e) {
             log.error(e.getMessage(), e);
             return null;
             //throw new RepocitoryCallException(RESULT_CODE.ERROR_READ_JSON);
 
         }
     }
-    public static <T>  T readValue(String josnStr, Class<T> cls){
-        if(StringUtils.isEmpty(josnStr)){
+
+    public static <T> T readValue(String josnStr, Class<T> cls) {
+        if (StringUtils.isEmpty(josnStr)) {
             return null;
         }
         try {
-            return (T)objectMapper.readValue(josnStr, cls);
+            return (T) objectMapper.readValue(josnStr, cls);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- fly4j-common/src/test/java/fly4j/common/test/TestJson.java
+++ fly4j-common/src/test/java/fly4j/common/test/TestJson.java
@@ -1,10 +1,15 @@
 package fly4j.common.test;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import fly4j.common.JsonUtils;
+import lombok.*;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 本地配置测试
@@ -13,31 +18,38 @@ import org.junit.Test;
 public class TestJson {
 
 
-    private String pin = "testpin";
-
     @Before
     public void setup() throws Exception {
-
-
     }
 
-
-    static private class TestJsonO {
-        private String priStr;
-        public String pubStr;
-
-        public TestJsonO(String priStr, String pubStr) {
-            this.priStr = priStr;
-            this.pubStr = pubStr;
-        }
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class User {
+        private String name;
+        private int age;
+        @JsonIgnore
+        private String remark;
     }
 
     @Test
     public void writeValueAsString2() throws Exception {
-        TestJsonO testJsonO = new TestJsonO("priStr", "pubStr");
-        String jsonStr = JsonUtils.writeValueAsString(testJsonO);
-//		System.out.println(jsonStr);
-        Assert.assertEquals("{\"pubStr\":\"pubStr\"}", jsonStr);
+
+        User user = new User("pin1", 20,"sdf");
+        String jsonStr = JsonUtils.writeValueAsString(user);
+        Assert.assertEquals("{\"name\":\"pin1\",\"age\":20}", jsonStr);
+        user= JsonUtils.readValue("{\"name\":\"pin1\",\"age\":20,\"age2\":20}", User.class);
+        Assert.assertEquals("pin1", user.getName());
+        Assert.assertEquals(20, user.getAge());
+
+        //Map
+        Map<String,String> map=new HashMap<>();
+        map.put("a","aa");
+        jsonStr=JsonUtils.writeValueAsString(map);
+        Assert.assertEquals("{\"a\":\"aa\"}", jsonStr);
+        map=JsonUtils.readStringStringHashMap(jsonStr);
+        Assert.assertEquals(1, map.size());
+        Assert.assertEquals("aa", map.get("a"));
 
     }
 
